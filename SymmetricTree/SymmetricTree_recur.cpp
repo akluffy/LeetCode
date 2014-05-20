42c234652bffeb07ca0acd8fbfa3af61f36f35ab
akluffy <akluffyusa@gmail.com>
2014-05-20 10:23:54 -0500

    recursion

---
+++ SymmetricTree/SymmetricTree_recur.cpp
@@ -0,0 +1,58 @@
+/*******************************************
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+**********************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+
+
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if(!root->left && !root->right) return true;
+        else if(root->left && root->right) return (isSymmetric(root->left) && isSymmetric(root->right));
+        else return false;
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
+
