f7aea7d2a3dd0c84d64e96c2fc15a7777ddbdcc5
akluffy <akluffyusa@gmail.com>
2014-06-09 00:23:21 -0500

    Recursion

---
+++ Restore IP Addresses/Restore IP Addresses.cpp
@@ -0,0 +1,65 @@
+/*******************************************
+
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+**********************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+#include <stack>
+#include <climits>
+
+
+using namespace std;
+
+
+
+
+
+void helper(string s, int start, int section, string solution, vector<string> &result) {
+    if(s.size() - start > (4-section) * 3) return;
+    if(s.size() < 4 - section) return;
+    if(start == s.size() && section == 4) {
+        solution.resize(solution.size() - 1);
+        result.push_back(solution);
+        return;
+    }
+
+    int digit = 0;
+    for(int i = start; i < start + 3; i++) {
+        digit = digit * 10 + (s[i] - '0');
+        if(digit <= 255) {
+            solution += s[i];
+            helper(s, i + 1, section + 1, solution + '.', result);
+        }
+        if(digit == 0) {break;}
+    }
+
+}
+
+vector<string> restoreIpAddresses(string s) {
+    vector<string> result;
+    string solution;
+    helper(s, 0, 0, solution, result);
+    return result;
+}
+
+
+int main()
+{
+
+
+
+    return 0;
+}
+
