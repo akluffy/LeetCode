7dbd67491577a5abf9051ad8ec8a57c013341945
JC <13722544@qq.com>
2015-04-01 17:38:07 -0500

    Revert "Revert "Revert "Update"""
    This reverts commit 53073d8bf759cea096e1606c13a6495a348a84c2.

--- Divide Two Integers/Divide Two Integers.cpp
+++ Divide Two Integers/Divide Two Integers.cpp
@@ -23,8 +23,8 @@ using namespace std;
         if(dividend < 0) sign = -sign;
         if(divisor < 0) sign = -sign;
 
-        unsigned long long dd = llabs(dividend);
-        unsigned long long ds = llabs(divisor);
+        unsigned long long dd = abs(dividend);
+        unsigned long long ds = abs(divisor);
         unsigned long c = 1;
 
         while(dd > ds) {
@@ -44,28 +44,6 @@ using namespace std;
 
         return result * sign;
     }
-
-
-    int divide2(int dividend, int divisor) {
-    if(divisor == 1) return dividend;
-    if(divisor == 0 || (dividend == INT_MIN && divisor == -1)) return INT_MAX;
-
-    long long a =  llabs(dividend), b = llabs(divisor);
-    long sign = (divisor > 0) ^ (dividend > 0) ? -1 : 1;
-    int result = 0;
-    int i = 0;
-    while(a >= b) {
-        i = 0;
-        while(a >= (b << i)) {
-            a -= (b << i);
-            result += (1 << i);
-            i++;
-        }
-    }
-
-
-    return (result * sign);
-}
 
 
 int main()
